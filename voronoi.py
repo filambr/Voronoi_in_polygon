79dd05b5e76846851187418a93e48127fe132e0a
filambr <104714764+filambr@users.noreply.github.com>
2023-03-01 14:57:54 +0200

    Create voronoi.py

---
+++ voronoi.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d
+from utils import polygon_intersection
+
+
+class Bounded_Voronoi():
+    def __init__(self, points, bounding_polygon):
+        
+        dummy_poitns = np.array([[-100, -100], [100, -100], [100, 100], [-100, 100]])
+        self.points = points
+        # np.vstack((points, dummy_poitns))
+        vor = Voronoi(np.vstack((points, dummy_poitns)))
+        self.regions = []
+        for i, region in enumerate(vor.point_region[0:-4]):
+            new_vor_region = polygon_intersection(vor.vertices[vor.regions[region]],
+                                                  bounding_polygon)
+            if new_vor_region is not None:
+                self.regions.append(new_vor_region)
+
+
+    def plot(self, axis = None):
+      if axis is None:
+          for i, region in enumerate(self.regions):
+              for ridge in ridges(region):
+                  plt.plot(ridge[:,0], ridge[:,1], color = f'C{i}')
+              plt.scatter(points[i][0], points[i][1], color = f'C{i}')
+              # plt.show()
+      else: 
+          for i, region in enumerate(self.regions):
+              for ridge in ridges(region):
+                  axis.plot(ridge[:,0], ridge[:,1], color = f'C{i}')
+              axis.scatter(self.points[i][0], self.points[i][1], color = f'C{i}')
+
+
+if __name__ == '__main__':
+# generates distorted pentagon
+    bounding_polygon = np.array([[-1, -1], [1, -1], [1.2, 1], [0, 1.2], [-1.2, 1]])+0.5+np.random.rand(5,2)*0.4
+
+    # n random points 
+    n = 5
+    points = np.random.rand(5,2)
+
+    vor = Bounded_Voronoi(points, bounding_polygon)
+    vor.plot()
+    plt.scatter(bounding_polygon[:,0], bounding_polygon[:,1], color = 'black')
+    plt.scatter(points[:,0], points[:,1],color='red')
+    plt.show()
